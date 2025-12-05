a3ed92f3c36d8a3a47b78a1fe13034defc9001bb
Manish kumar <169014705+manishkumar2543@users.noreply.github.com>
2025-12-05 21:50:11 +0530

    Day-2

--- script.js
+++ script.js
@@ -1,109 +1,110 @@
-//  Understanding 'classes' and 'object' in Javascript.
-
-// Rule Number
-// -jab bhi hm class bnyege to class bad vla leter cpaital later me hi likhe pahila later
-// EX--  class BiscuitMaker{};
-
-// 2. jab constructor chlta hai to (this) ki value ek nya object hota hai.
-
-
-// constructor✍💡
-// class BiscuitMaker {
-//     constructor (){
-//         this.name='ParleG';
-//         this.price =5;
-
-//     }
-// }
-// ab class ko chlayenge ✍
-// new- ka mtlb blank object✍
-// let biscuit- iska mtlb jo (this) ek blank tha usko rakhne ke liye isko bnye hai>✍
-
-//  let biscuit=new BiscuitMaker();
-
-
-
-//  Class bnnate hai taaki hme har baar ek naya object mil jaye
-
-// classes object ki factory hai
-
-// har baar new word ke saath class run karoge to ek nya object milega.
-
-// constuctor-- ek function hota hai.
-// class ChooleChaat{
+// class Sketch{
 //     constructor(){
-//         this.price=55;
-//         this.oil="5ml";
-//         this.oilBrand="Saffola";
-//         this.mashala=['dhaniya', 'pudina', 'mirch'];
-//     }
+//         this.character="doremon";
+//         this.color="blue";
+//         this.someFnc = function () {};
 
-//     pack(){
-//         console.log('aalo chaat packed');
-//     }
-//     unpack(){
-//         console.log('aalo chaat unpacked')
 //     }
 // }
-// class AlooChaat{
-//     constructor(){
-//         this.price=55;
-//         this.oil="5ml";
-//         this.oilBrand="Saffola";
-//         this.mashala=['dhaniya', 'pudina', 'mirch'];
-//     }
+// Sketch.prototype.speak = function () {};
+// Sketch.prototype.walk = function () {};
+// let Sketch1=new Sketch();
 
-//     pack(){
-//         console.log('aalo chaat packed');
-//     }
-//     unpack(){
-//         console.log('aalo chaat unpacked')
+// prototype- is a shared memory
+
+
+//   this Keyword  -📢✍✍
+
+// global  ->  window
+// function -> window
+// es5 function inside object -> object
+// es5 function inside es5 function inside object -> window
+// es6 function inside es5 function inside object -> object
+
+
+// console.log(this)
+// output- window👈
+
+
+// function abcd (){
+//     console.log(this)
+// }
+// abcd():
+// output - window👈
+
+
+
+// let obj= {
+//     name: "manish",
+//     fnc: function(){
+//         console.log(this)
 //     }
 // }
-// // New- jab bhi (new) ke sath koe object bnyange to constructor chl jayega💡✍✍✍
-//  let ac1=new AlooChaat() 
-//  ac1.pack();
+// obj.fnc();
+// output - Object  👈👈
 
 
+// let obj={
+//     name:"maisn",
+//     fnc:()=>{
+//         console.log(this);
+//     },
+// };
 
-// class Kitaab{
-//     constructor(name, price,author,colro ){
-//         this.name=name;
-//         this.price=price;
-//         this.author=author;
-//         this.colro=colro;
-//     }
-//     pannaPalto(){
-//         console.log("")
-//     }
-//     bookmarkLagao(){}
-//     padhlo(){}
+// obj.fnc();
+// Output - window👈👈
+
+
+// es5 function inside es5 function inside object -> window
+
+// let obj={
+//     name:"manish",
+//     fnc: function(){
+//         function abcd(){
+//             console.log(this)
+//         }
+//         abcd();
+//     },
+    
+// };
+// obj.fnc();
+
+
+// Call , apply, bind 📢✍
+
+// ek function me (this ) ki vlaue window hoti hai, agr app chaahte ho ki  vlaue window naa ho ki wo value window naa ho 
+// par koe aur object ho tb app use kr skte ho call apply bind ka.
+
+// call✍💡
+// let obj={
+//     name:"manish",
+// };
+// function abcd(){
+//     console.log(this);
 // }
-// let k1=new Kitaab("Gk",200,'manish','white');
-// let k2=new Kitaab('computer',240,'sonu','red');
-// let k3=new Kitaab('Maths', 350, 'Rk singh','blue');
+// abcd.call(obj);
+
+// apply💡✍
+// let obj={
+//     name:"manish",
+// }
+// function abcd(a,b,c){
+//     console.log(this,a,b,c)
+// };
+// abcd.apply(obj,[1,2,3])
 
 
-// Constructor ->  Ek function jo automatic chalta hai jaise hi class se naya instance bnaya jata hai✍
-// instance - ka mtlb ek sacha 
-// ex- k1 ek instance hai mtlb class ki ek copy. and agr hm (new.Kitaab likhe to direct chl jaye usi time)
-// let k1 = new Kitaab(); 
+// bind
 
-// Prototypes - [this keyword, call, apply, bind]📢📢📢
+// let obj={
+//     name:'manish',
+// }
+// function abcd(a,b,c){
+//     console.log(this,a,b,c);
+// }
+// let newfnc= abcd.bind(obj,1,2,3);
+// newfnc();
 
-class Human {
-    constructor(){
-        this.name='Manish';
-        this.age=23;
-    }
-}
-Human.prototype.saasLo = function(){
-    console.log("hey");
-}
-Human.prototype.khaanaKhao = function(){
-    console.log("hey")
-}
-let h1=new Human();
-let h2=new Human();
-
-
+// call -> fnc chaltata hai and this ki vlaue set karta hai .
+// apply -> wahi karta hai jo call karta hai and arguments main pahli vluae this ki and doosri vlaue arry hot hai .👈
+// bind -> wahi krta hai jo call krta hai and apko nya fnc deta hai👈
