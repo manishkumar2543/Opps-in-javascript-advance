5fd92b0fd83c2038fcf904cd25061fc8b2e03447
Manish kumar <169014705+manishkumar2543@users.noreply.github.com>
2025-12-09 20:58:43 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,46 +1,123 @@
-// - Introduction to Asynchrony in Javascript.
-// - Introduction to 'callbacks' and Problems in Callbacks
-// - Understanding 'promises' - 'pending' 'resolved' , 
+// Exercise 1 (very easy - warming up)
+// Ek function banao afterDelay
+// Requirements:
+// Ye funtion 2 cheezein lega:
+// time (milliseconds)
+// callback function 
+// Given time ke baad callback call kare
+// Callback ke andar "Callback executed" print hona chahiye
+// Use case socho:
+// "2 second baad lek kam karna hai"
+// Goal:
+// Samjhna ki callback delay ke baad kaise execute hota hai
+// Ye setTimeout + callback connection hai
 
-
-
-// js  -> single threaded hai
-// ek kaam ek baar main kar paayegi
-
-// synchronous approach
-// asynchronous approach
-
-
-// - Introduction to 'callbacks' and Problems in Callbacks
-// callback -> ek function jo turnat nhi chalega ye chalega jab koe kam complete hoga.
-
-// setTimeout(function(){
-//     console.log("Hey")
-// },2000)
-
-
-
-
-// function abcd(fn){
-//     fn(function (fn3){
-//         fn3(function (fn5){
-//             fn5()
-//         });
-//     })
+// function afterDelay(time,cb){
+//     setTimeout(()=>{
+//         cb();
+//     },time)
 // }
-// abcd(function(fn2){
-//    fn2(function(fn4){
-//     console.log("holoel")
-//    })
+// afterDelay(3000,function(){
+//     console.log("callback executed")
+// });
+
+// Exercise 2 (Intermediate -data flow)
+// Ek function banao getUser
+// Requirements :
+// 1 second ke baad callback ko ek object de:
+// id
+// username
+// Then
+// Callback ke ander ek aur function call karo getUserPosts
+// userId lega
+// 1 second ke baad callback to posts ka array de
+// Final output:
+// User ka username print ho
+// fir uske posts print ho 
+// Goal:
+// Samajhna ki ek async ka result next ko kasie milta hai.
+
+// function getUser(username,cb){
+//     console.log("getting user details...")
+//     setTimeout(()=>{
+//         console.log("geting user details...")
+//         cb({id:1, username:"Manish"})
+//     },1000)
+// }
+
+// function getUserPosts(id,cb){
+//     setTimeout(()=>{
+//         cb(["helo","good da", "fack you"]);
+//     },2000)
+// }
+//  getUser("Manhsi",function(data){
+//     getUserPosts(data.id, function(allposts){
+//         console.log(data.username, allposts)
+//     });
+//  });
+
+//2. 👈
+
+// function instagramSeDataLaao(username,cb){
+//     setTimeout(()=>{
+//         cb({uniquenum: 1234, username: "Manish"});
+//     },2000)
+// }
+
+// function metaPeJaaoDataLaao(uniqunum,cb){
+//     setTimeout(()=>{
+//         cb(["img1","img2"]);
+//     },4000)
+// }
+// instagramSeDataLaao("Manish",function(data){
+//     metaPeJaaoDataLaao(data.uniqunum,function(imges){
+//         console.log(imges)
+//     })
 // })
 
 
-function amintseDetailslao(address, cb){
-    console.log("fetching details...")
+// Exercise 3 (Intermediate - callback dependency, thoda painful)📢
+// Teen functions banao:
+// loginUser
+// 1 second baad callback to user object de fetchPermissions
+// userId lega
+// 1 second baad callback ko permissions array de
+// loadDashboard
+// permission lega
+// 1 second baad callback to "Dashboard loaded" bole 
+// Flow:
+// Pehle loginUser
+// Uske andar fetchPermissions
+// Uske andar loadDashboard
+// Final output console mein print ho 
+// Gaol:
+// Callback nesting ko feel karna
+// Yhi structure baad mein callback hell bante hai.
+
+function loginUser(username, cb){
+     console.log("logging in user...")
     setTimeout(()=>{
-        cb({lat:23.33, lang:234.444});
-    },3000)
-};
-amintseDetailslao("siwan chock road",function(deatils){
-    console.log(deatils)
+        cb({id:12, username:"manish"});
+    },1000)
+}
+
+function fetchPermissions(id,cb){
+   console.log("fetching permission...")
+    setTimeout(()=>{
+        cb(["read","write","delete"])
+    },2000)
+}
+function loadDashboard(permission,cb){
+    console.log("loading dashboard...")
+    setTimeout(()=>{
+        cb();
+    },2000)
+}
+
+loginUser("Manish",function(userdata){
+    fetchPermissions(userdata.id, function(permission){
+        loadDashboard(permission,function(){
+            console.log("😚🤑dashboard loaded")
+        })
+    })
 })
